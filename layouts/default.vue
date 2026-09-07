d598009ad7618de130593f8dd5ba31e0f0fef852
mani-kandan-k <107452926+mani-kandan-k@users.noreply.github.com>
2026-09-07 21:15:38 +0530

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex min-h-screen flex-col bg-base-100 text-base-content">
+    <AppNavbar />
+    <main class="flex-1">
+      <slot />
+    </main>
+    <AppToasts />
+  </div>
+</template>
